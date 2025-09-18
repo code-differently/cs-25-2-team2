9ec747ecf87b39ef003049f3efa4a8585f27c7d4
brooklynharden <hardenbrooklyn123@gmail.com>
2025-09-18 13:47:43 +0000

    feat:created file for CartItem, and its class and constructor

---
+++ src/main/java/com/cs_25_2_team2/RestaurantManagementApp/CartItem.java
@@ -0,0 +1,12 @@
+package com.cs_25_2_team2.RestaurantManagementApp;
+
+public class CartItem {
+    private MenuItem menuItem;
+    private int quantity;
+
+    public CartItem(MenuItem menuItem, int quantity){
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+    
+}
